a56454778c1bfb93718f98ac8eec61e3c7ce95d1
sschwarz25 <sschwarz25@gmail.com>
2017-04-29 18:08:16 -0500

    asd
    asd

--- src/TCP.java
+++ src/TCP.java
@@ -1,9 +1,11 @@
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -18,7 +20,7 @@ public class TCP {
 
 	try ( Socket socket = new Socket ( IP, portNumber );
 		InputStream inFile = new FileInputStream ( file );
-		InputStream in = socket.getInputStream ();
+		BufferedReader in = new BufferedReader ( new InputStreamReader ( socket.getInputStream () ) );
 		PrintWriter out = new PrintWriter ( socket.getOutputStream (), true );
 		ByteArrayOutputStream baos = new ByteArrayOutputStream (); ) {
 
@@ -30,8 +32,7 @@ public class TCP {
 	    int packetCount = ( int ) Math.ceil ( file.length () / buffer.length );
 
 	    // Convert to String, then bytes, then write to socket
-	    baos.write ( Integer.toString ( packetCount ).getBytes () );
-	    out.println ( baos.toByteArray () );
+	    out.println ( packetCount );
 
 	    // Console the Packet Size
 	    System.out.println ( "Sending " + packetCount + " Packets." );
@@ -39,16 +40,12 @@ public class TCP {
 	    // ------------WAIT ON SERVER
 
 	    // Get Packet Count ACK and Give File Name
-	    in.read ( buffer );
-	    String bufferString = new String ( buffer );
-	    bufferString.trim ();
+	    String bufferString = in.readLine ();
 
 	    if ( bufferString.contains ( Integer.toString ( packetCount ) ) ) {
 		System.out.println ( "Server ACK Packet Count: " + bufferString );
 
 		// Tell Server File Name
-		baos.flush ();
-		out.flush ();
 		baos.write ( file.getName ().getBytes () );
 		out.println ( baos.toByteArray () );
 
@@ -70,26 +67,21 @@ public class TCP {
 
 	try ( ServerSocket server = new ServerSocket ( portNumber );
 		Socket socket = server.accept ();
-		InputStream in = socket.getInputStream ();
-		PrintWriter out = new PrintWriter( socket.getOutputStream () );
+		BufferedReader in = new BufferedReader ( new InputStreamReader ( socket.getInputStream () ) );
+		PrintWriter out = new PrintWriter ( socket.getOutputStream () );
 		OutputStream fileOut = new FileOutputStream ( fileName );
 		ByteArrayOutputStream baos = new ByteArrayOutputStream (); ) {
 
-
-	    in.read ( buffer );
-	    String packetCountString = new String ( buffer );
+	    String packetCountString = in.readLine ();
 	    System.out.println ( "Incoming Packets: " + packetCountString );
 
 	    int packetCount = Integer.parseInt ( packetCountString.trim () );
 
 	    // ACK Packet Count
-	    baos.write ( Integer.toString ( packetCount ).getBytes () );
-	    out.println ( baos.toByteArray () );
+	    out.println ( packetCount );
 
 	    // Get File Name
-	    in.read ( buffer );
-	    String fileNameFromClient = new String ( buffer );
-	    fileName.trim ();
+	    String fileNameFromClient = in.readLine ();
 
 	    System.out.println ( "File Name: " + fileNameFromClient );
 
@@ -99,8 +91,10 @@ public class TCP {
 	    }
 
 	    int count = 1;
-	    while ( ( count = in.read ( buffer ) ) > 0 ) {
-		fileOut.write ( buffer, 0, count );
+	    String read = "";
+	    
+	    while ( (read = in.readLine ()) != null) {
+		fileOut.write ( read.getBytes (), 0, count );
 	    }
 
 	} catch ( IOException e ) {
